dcc58e55beb483fe432864a6f47356fbcc892632
swsong <sw930601@gmail.com>
2021-08-31 16:23:33 +0900

    slack bot test

--- config/slack.py
+++ config/slack.py
@@ -2,7 +2,7 @@ import requests
 
 class Slack():
     def __init__(self):
-        self.token = 'xoxb-2421660935986-2443869055060-TKFRMoGi1OIB0x4i2uguLBGT'
+        self.token = 'xoxb-2421660935986-2443869055060-onl0bwLP6FeKPuAHsRswRmPg'
         self.channel = '#bot_test'
 
     def notification(self, text=None):
